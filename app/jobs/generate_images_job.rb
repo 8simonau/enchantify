28f4369bbce7b4134e951ea99724146e185f8108
ulrich aguey <ulrichatlanta@hotmail.com>
2024-02-22 21:02:21 +0100

    genrerate image job

---
+++ app/jobs/generate_images_job.rb
@@ -0,0 +1,15 @@
+class ImagesJob < ApplicationJob
+  queue_as :default
+
+  def perform(story)
+    # Do something later
+
+    curl_command = %Q(curl -X POST "https://api.openai.com/v1/images/generations" \
+                    -H "Content-Type: application/json" \
+                    -H "Authorization: Bearer #{ENV.fetch 'OPENAI_API_KEY'}" \
+                    -d '{"model": "dall-e-3", "prompt": "#{Story.text}", "n": 1, "size": "1024x1024"}')
+
+    response = `#{curl_command}`
+    puts response
+  end
+end
