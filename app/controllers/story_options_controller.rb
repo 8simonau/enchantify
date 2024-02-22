f43c596bfd2670379b4840d35c7a03fc2535796a
Aurelien Simon <aureliensimon@hotmail.com>
2024-02-22 22:11:24 +0100

    finalized tickmark

--- app/controllers/story_options_controller.rb
+++ app/controllers/story_options_controller.rb
@@ -7,7 +7,12 @@ class StoryOptionsController < ApplicationController
   def create
     @story = Story.find(params[:story_id])
     @option = Option.find(params[:option])
-    @story_option = StoryOption.new(option_id: params[:option], story_id: params[:story_id])
-    @story_option.save!
+    # if StoryOption already exits, then destroys
+    if @story.options.include?(@option)
+      StoryOption.find_by(story_id: params[:story_id], option: params[:option]).destroy
+    else
+      @story_option = StoryOption.new(option_id: params[:option], story_id: params[:story_id])
+      @story_option.save!
+    end
   end
 end
