82ba242a50263c66cd86c0caad31dc17fb00c213
emmanuel bommelaer <emmanuel.bommelaer@gmail.com>
2024-02-24 13:09:26 +0100

    build image prompt from ttext response

--- app/jobs/generate_images_job.rb
+++ app/jobs/generate_images_job.rb
@@ -1,33 +1,40 @@
 class GenerateImagesJob < ApplicationJob
   queue_as :default
 
-    def perform(story)
-      # Do something later
-      url = "https://api.openai.com/v1/images/generations"
-      body = {
-        "model": "dall-e-3",
-        "prompt": "#{story.text}",
-        "size": "1024x1024"
-      }
+  def perform(story, prompt)
+    # Do something later
+    preprompt = "Vous êtes un talentueux illustrateur de contes pour enfants qui dessine au pinceau des illustrations. Celles-ci doivent être colorées et belles, et stimuler l'imagination des enfants. Chaque illustration est créée à l'aide d'un personnage principal (un enfant), un lieu et un objet magique. Une phrase décrit l'action précise. Voici les paramètres: "
+    story.options_hash.each do |k, v|
+      preprompt << "#{k} : #{v}. "
+    end
+    full_prompt = preprompt + " Voici l'action à décrire : " + prompt
 
-      response = Faraday.post(url) do |req|
-        req.headers['Content-Type'] = 'application/json'
-        req.headers['Authorization'] = 'Bearer ' + ENV.fetch("OPENAI_KEY")
-        req.body = body.to_json
-      end
+    url = "https://api.openai.com/v1/images/generations"
+    body = {
+      "model": "dall-e-3",
+      "prompt": full_prompt,
+      "size": "1024x1024"
+    }
 
-      puts ""
-      puts "*******************************"
-      puts ""
-      puts "IMAGE GEN RESPONSE:"
-      puts ""
-      puts response.body
-      puts ""
-      puts "*******************************"
-      puts ""
+    response = Faraday.post(url) do |req|
+      req.headers['Content-Type'] = 'application/json'
+      req.headers['Authorization'] = 'Bearer ' + ENV.fetch("OPENAI_KEY")
+      req.body = body.to_json
+    end
 
-      # story.audio.attach(io: StringIO.new(response.body), filename: "audio.mp3", content_type: "audio")
-      # story.save!
-      response
+    puts ""
+    puts "*******************************"
+    puts ""
+    puts "IMAGE GEN RESPONSE:"
+    puts ""
+    puts response.body
+    puts ""
+    puts "*******************************"
+    puts ""
+    return response
+
+    url = URI.open()
+    story.pictures.attach(io: StringIO.new(response.body), filename: "#{prompt}.jpg", content_type: "image/jpeg")
+    story.save!
   end
 end
