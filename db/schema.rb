6c31852c07923d907a2fa74e0d5fd9ce96ee16f6
ulrich aguey <ulrichatlanta@hotmail.com>
2024-03-07 20:48:20 +0100

    icons

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_24_164647) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_02_082551) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -95,6 +95,9 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_24_164647) do
     t.string "last_name"
     t.boolean "admin", default: false, null: false
     t.bigint "active_child_id"
+    t.string "elevenlabs_key"
+    t.string "cloudinary_url"
+    t.string "openai_key"
     t.index ["active_child_id"], name: "index_users_on_active_child_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
